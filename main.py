e6f093be36c90b213e1a27ac2bb4617f59488198
Tomasz Madej <tomasz@madej.ovh>
2022-10-02 12:08:00 +0200

    Add body

--- main.py
+++ main.py
@@ -16,4 +16,42 @@ def multiplication(a,b):
 def division(a,b):
     logging.info(f'Dzielę {a} / {b}')
     print('Wynik z dokładnością do dwóch miejsc po przecinku to: ',round(a / b,2))
-    
+
+if __name__ == "__main__":
+
+    x = int(input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: '))
+
+    if x == 1:
+        c = int(input('Podaj ile składników chcesz dodać do siebie: '))
+        if c < 2:
+            print('Nie można dodać mniej niż dwóch składników.')
+        if c == 2:
+            a = float(input('Podaj składnik 1: '))
+            b = float(input('Podaj składnik 2: '))
+            addition(a ,b)
+        elif c >2:
+            addition_more_than_2(c)
+
+    if x == 2:
+        a = float(input('Podaj składnik 1: '))
+        b = float(input('Podaj składnik 2: '))
+        subtraction(a, b)
+
+    if x == 3:
+        c = int(input('Podaj ile składników chcesz pomnożyć przez siebie: '))
+        if c < 2:
+            print('Nie można mnożyć mniej niż dwóch składników.')
+        if c == 2:
+            a = float(input('Podaj składnik 1: '))
+            b = float(input('Podaj składnik 2: '))
+            multiplication(a,b)
+        elif c > 2:
+            multiplication_more_than_2(c)
+
+    if x == 4:
+        a = float(input('Podaj składnik 1: '))
+        b = float(input('Podaj składnik 2: '))
+        division(a, b)
+
+    elif x <1 or x > 4:
+        print('Podano błędny numer.')
