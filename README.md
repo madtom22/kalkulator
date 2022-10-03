16bb0362c17dd61e22798efaa8ade5a6d40882f3
Tomasz Madej <tomasz@madej.ovh>
2022-10-03 21:28:44 +0200

    Add new version

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Kalkulator ma 4 podastawowe funkcję - dodawanie, odejmowanie, mnożenie i dzielenie.
-Przy odejmowaniu i dzieleniu użytkownik może podać tylko 2 składiki, natomiast przy dodawania i mnożenia można podać więcej składników niż 2 i zastosowanu tu warunek sprawdzenia podania ilości składników - nie może być ich mniej niż 2.
-W definicji funkcji dzielenia dodany został opis wyjatku przy dzieleniu przez 0.
+W tej modyfikacji zmniejszyłem ilość funkcji do 4 - bo tam gdzie można podać 2 lub więcej składników można to zrobić za pomocą tej samej funkcji - wcześniej sugerowałem się ładnym wyświetleniem funkcji log przy dwóch składnikach ale można to pominąć :)
+Przeniosłem do poszczególnych funkcji wymaganą interakcję z użytkownikiem.
+Uprościłem kod w __main__ przyjmując z góry c=2 - ilość składników (według założeń z zadania) - a tam gdzie jest możliwość więcej składników użytkownik podaje wartość c - dzięki temu niektóre if == x mają tylko jedną linijkę kodu :)
--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import logging
 logging.basicConfig(level=logging.INFO)
 
-def addition(a,b):
-    logging.info(f'Dodaję {a} + {b}')
-    print('Wynik z dokładnością do dwóch miejsc po przecinku to: ', round(a + b,2))
-
-def addition_more_than_2(c):
+def addition(c):
     e = 0
     f = []
     for i in range(1,c+1):
@@ -15,15 +11,13 @@ def addition_more_than_2(c):
     logging.info(f'Składnik które zostały dodane do siebie: {f}')
     print('Wynik z dokładnością do dwóch miejsc po przecinku to: ', round(e,2))
 
-def subtraction(a,b):
+def subtraction(c):
+    a = float(input('Podaj składnik 1: '))
+    b = float(input('Podaj składnik 2: '))
     logging.info(f'Odejmuję {a} - {b}')
     print('Wynik z dokładnością do dwóch miejsc po przecinku to: ', round(a - b,2))
 
-def multiplication(a,b):
-    logging.info(f'Mnożę {a} * {b}')
-    print('Wynik z dokładnością do dwóch miejsc po przecinku to: ', round(a * b,2))
-
-def multiplication_more_than_2(c):
+def multiplication(c):
     e = 1
     f = []
     for i in range(1,c+1):
@@ -33,7 +27,9 @@ def multiplication_more_than_2(c):
     logging.info(f'Składnik które zostały pomnożone przez siebie to: {f}')
     print('Wynik z dokładnością do dwóch miejsc po przecinku to: ', round(e,2))
 
-def division(a,b):
+def division(c):
+    a = float(input('Podaj składnik 1: '))
+    b = float(input('Podaj składnik 2: '))
     try:
         logging.info(f'Dzielę {a} / {b}')
         print('Wynik z dokładnością do dwóch miejsc po przecinku to: ', round(a / b,2))
@@ -43,38 +39,27 @@ def division(a,b):
 if __name__ == "__main__":
 
     x = int(input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: '))
+    c = 2
 
     if x == 1:
         c = int(input('Podaj ile składników chcesz dodać do siebie: '))
         if c < 2:
             print('Nie można dodać mniej niż dwóch składników.')
-        if c == 2:
-            a = float(input('Podaj składnik 1: '))
-            b = float(input('Podaj składnik 2: '))
-            addition(a ,b)
-        elif c >2:
-            addition_more_than_2(c)
+        elif c >= 2:
+            addition(c)
 
     if x == 2:
-        a = float(input('Podaj składnik 1: '))
-        b = float(input('Podaj składnik 2: '))
-        subtraction(a, b)
+        subtraction(c)
 
     if x == 3:
         c = int(input('Podaj ile składników chcesz pomnożyć przez siebie: '))
         if c < 2:
             print('Nie można mnożyć mniej niż dwóch składników.')
-        if c == 2:
-            a = float(input('Podaj składnik 1: '))
-            b = float(input('Podaj składnik 2: '))
-            multiplication(a,b)
-        elif c > 2:
-            multiplication_more_than_2(c)
+        elif c >= 2:
+            multiplication(c)
 
     if x == 4:
-        a = float(input('Podaj składnik 1: '))
-        b = float(input('Podaj składnik 2: '))
-        division(a, b)
+        division(c)
 
     elif x < 1 or x > 4:
         print('Podano błędny numer.')
